7bda0594fdc08424568a8d1df8739744332a30fd
Harcharan Singh <artisangang@gmail.com>
2017-09-13 21:21:54 +0530

    file rule

---
+++ test/custom.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+
+const should = require('should');
+
+const Validator = require('../index');
+
+let r = {};
+
+describe('Custom Rules', function() {
+
+	describe('custom', function() {
+
+	    it('should return true', async () => {
+
+
+	    	let v = new Validator(r, 
+	    		{remember:'yes'}, {remember: 'custom'});
+
+	    	v.rules.custom = async (field, value, message)  => {
+	    	
+		        if( value === 'yes' || value === 'on' ){
+		            return true;
+		        }else{
+		            this.validator.addError(field, 'custom', message || 'The value of the field needs to be  yes or no');
+		            return false;
+		        }
+    
+	    	};
+
+	    	let matched = await v.check();
+
+	    	assert.equal(matched, true);
+	      	
+
+	    });
+	     
+	     it('should return false', async () => {
+
+
+	    	let v = new Validator(r, 
+	    		{remember:'1'}, {remember: 'custom'});
+
+	    	v.rules.custom = async (field, value, message)  => {
+	    	
+		        if( value === 'yes' || value === 'on' ){
+		            return true;
+		        }else{
+		            v.addError(field, 'custom', message || 'The value of the field needs to be  yes or no');
+		            return false;
+		        }
+    
+	    	};
+
+	    	let matched = await v.check();
+
+	    	assert.equal(matched, false);
+	      	
+
+	    });
+	     
+
+	});
+});
