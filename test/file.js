5262730e02da7484aee157773d68e3561083d503
Harcharan Singh <artisangang@gmail.com>
2017-09-14 18:32:42 +0530

    filesize

---
+++ test/file.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+
+const should = require('should');
+
+const Validator = require('../index');
+
+const fs = require('fs');
+
+let r = {};
+
+describe('File Rules', function() {
+
+	describe('size', function() {
+
+	    it('should return true', async () => {
+
+
+	    	let v = new Validator(r, 
+	    		{file:fs.readFileSync('./test/files/file-small.png')}, {file: 'size:4kb'});
+	    	
+
+	    	let matched = await v.check();
+
+	    	console.log(v.errors);
+
+	    	assert.equal(matched, true);
+	      	
+
+	    });
+	     
+	     it('should return false', async () => {
+	      	
+	      	let v = new Validator(r, 
+	    		{file:fs.readFileSync('./test/files/file-small.png')}, {file: 'size:1kb'});
+	    	
+
+	    	let matched = await v.check();
+
+	    	console.log(v.errors);
+
+	    	assert.equal(matched, false);
+
+	    });
+	     
+
+	});
+});
