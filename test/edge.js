77c23ba00b3136c7842557f932ce1802c7bf93f2
Jakub Nietrzeba <jakub@connecteddigital.com>
2017-10-25 12:42:40 +0200

    Do not throw exceptions on null values
    - add edge cases test
    - refactor code with use of new helper function

---
+++ test/edge.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+
+const should = require('should');
+
+const Validator = require('../index');
+
+let r = {};
+
+describe('Edge Cases', function () {
+
+    describe('undefined', function () {
+
+        it('should ignore undefined and not required fields', async () => {
+
+            let v = new Validator(r, {field: undefined}, {field: 'string'});
+
+            let matched = await v.check();
+            assert.equal(matched, true);
+
+        });
+
+
+        it('should reject undefined and required fields', async () => {
+
+            let v = new Validator(r, {field: undefined}, {field: 'required|string'});
+
+            let matched = await v.check();
+            assert.equal(matched, false);
+
+        });
+
+    });
+
+    describe('null', function () {
+
+        it('should ignore null and not required fields', async () => {
+
+            let v = new Validator(r, {field: null}, {field: 'string'});
+
+            let matched = await v.check();
+            assert.equal(matched, true);
+
+        });
+
+
+        it('should reject null and required fields', async () => {
+
+            let v = new Validator(r, {field: null}, {field: 'required|string'});
+
+            let matched = await v.check();
+            assert.equal(matched, false);
+
+        });
+
+    });
+
+    describe('empty string', function () {
+
+        it('should ignore empty string in not required fields', async () => {
+
+            let v = new Validator(r, {field: ''}, {field: 'string'});
+
+            let matched = await v.check();
+            assert.equal(matched, true);
+
+        });
+
+
+        it('should reject empty string in required fields', async () => {
+
+            let v = new Validator(r, {field: ''}, {field: 'required|string'});
+
+            let matched = await v.check();
+            assert.equal(matched, false);
+
+        });
+
+    });
+
+});
