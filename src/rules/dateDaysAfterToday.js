e8aaa4257167c3ad6d5dae757a5b60e651ff4ccb
Sergi Baños <sergi.banos@wefoxgroup.com>
2019-02-22 18:53:11 +0100

    Added methods of dates
    dateDaysAfterToday, dateYearsAfterToday, dateDaysBeforeToday, dateYearsBeforeToday

---
+++ src/rules/dateDaysAfterToday.js
@@ -0,0 +1,18 @@
+const moment = require('moment'),
+    { dateFormats } = require('../lib/date');
+
+module.exports = async function dateDaysAfterToday(field, value, days) {
+
+
+    let mAfterDate, mDate;
+
+    mAfterDate = moment('', dateFormats).add(days, 'days');
+    mDate = moment(value, dateFormats);
+
+    /* istanbul ignore next */
+    if (!mAfterDate.isValid() || !mDate.isValid() || mAfterDate.valueOf() > mDate.valueOf()) {
+        return false;
+    }
+
+    return true;
+}
---
+++ src/rules/dateYearsAfterToday.js
@@ -0,0 +1,18 @@
+const moment = require('moment'),
+    { dateFormats } = require('../lib/date');
+
+module.exports = async function dateYearsAfterToday(field, value, days) {
+
+
+    let mAfterDate, mDate;
+
+    mAfterDate = moment('', dateFormats).add(days, 'years');
+    mDate = moment(value, dateFormats);
+
+    /* istanbul ignore next */
+    if (!mAfterDate.isValid() || !mDate.isValid() || mAfterDate.valueOf() > mDate.valueOf()) {
+        return false;
+    }
+
+    return true;
+}
