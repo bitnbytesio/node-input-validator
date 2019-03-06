cac38800f920b4da39fb1859cf3d3560a132a1a5
artisangang <artisangang@gmail.com>
2019-03-06 12:40:15 +0530

    test case and ver upgrade

---
+++ test/rules/acceptedNotIf.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+
+const Validator = require('../../index');
+
+const messages = require('../../src/messages/en/messages');
+
+let r = {};
+
+
+describe('acceptedNotIf', function () {
+
+    it('validation should pass: with yes', async () => {
+
+        const v = new Validator(
+            { attribute: 'no', age: 16 },
+            { attribute: 'acceptedNotIf:age,16' }
+        );
+
+        const matched = await v.check();
+
+        assert.equal(matched, true);
+
+    });
+
+      
+    it('validation should fail', async () => {
+
+        const v = new Validator(
+            { attribute: 'yes', age: 16 },
+            { attribute: 'acceptedNotIf:age,16' }
+        );
+
+        const matched = await v.check();
+
+        assert.equal(matched, false);
+
+        assert.equal(v.errors.attribute.message, v.parseExistingMessageOnly('acceptedNotIf', 'attribute', 'yes', ['age', '16']));
+
+    });
+
+
+});
