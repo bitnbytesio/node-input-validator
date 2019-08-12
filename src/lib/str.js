b6fa68d7fc59357d4b1f39ac1d3cbca9e36b87d1
Harcharan Singh <artisangang@gmail.com>
2019-08-12 08:06:30 -0500

    v3.6.4

--- src/lib/str.js
+++ src/lib/str.js
@@ -1,5 +1,5 @@
 exports.camelToSentance = function camelToSentance(str) {
-  return str.replace(/([A-Z]+)/g, ' $1').trimStart().toLowerCase();
+  return str.replace(/([A-Z]+)/g, ' $1').trimLeft().toLowerCase();
 };
 
 exports.snakeToSentance = function snakeToSentance(str) {
