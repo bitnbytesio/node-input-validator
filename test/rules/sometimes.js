7024b7706a3793cee99671f6f3c3a84c6a0c49a0
Larry <larryteal@163.com>
2020-07-15 19:10:35 +0800

    Rule sometimes add deep path support

--- test/rules/sometimes.js
+++ test/rules/sometimes.js
@@ -58,4 +58,58 @@ describe('sometimes', () => {
       }),
     );
   });
+
+  it('should fail', async () => {
+    const v = new Validator(
+      { info: { password: '', confirm_password: 'password' } },
+      { 'info.password': 'sometimes', 'info.confirm_password': 'sometimes|alpha' },
+    );
+
+    const matched = await v.check();
+
+    assert.equal(matched, false);
+  });
+
+  it('should pass', async () => {
+    const v = new Validator(
+      { info: { password: 'password', confirm_password: 'password' } },
+      { 'info.password': 'sometimes', 'info.confirm_password': 'sometimes|alpha' },
+    );
+
+    const matched = await v.check();
+    assert.equal(matched, true);
+  });
+
+  it('should pass, attribute absent', async () => {
+    const v = new Validator(
+      {},
+      { 'info.password': 'sometimes', 'info.confirm_password': 'sometimes|alpha' },
+    );
+
+    const matched = await v.check();
+
+    assert.equal(matched, true);
+  });
+
+  it('message should exist', async () => {
+    const v = new Validator(
+      { info: { password: '', confirm_password: 'password' } },
+      { 'info.password': 'sometimes', 'info.confirm_password': 'sometimes|alpha' },
+    );
+
+
+    const matched = await v.check();
+
+    assert.equal(matched, false);
+
+    assert.equal(
+      v.errors['info.password'].message,
+      v.getExistinParsedMessage({
+        rule: 'sometimes',
+        value: '',
+        attr: 'info.password',
+        args: [],
+      }),
+    );
+  });
 });
