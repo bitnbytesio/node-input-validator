f0f5eab3b524af5be57fca27a6109d0b2796db5c
Harcharan Singh <bitnbytesio@gmail.com>
2020-07-25 22:42:58 +0530

    rule:requiredIf

--- src/rules/required.rule.ts
+++ src/rules/required.rule.ts
@@ -1,4 +1,4 @@
-import { ValidationRuleContract } from "../contracts";
+import { ValidationRuleContract, ValidatorContract } from "../contracts";
 
 import { reallyEmpty } from "../utils/ops.util";
 
@@ -7,6 +7,43 @@ export function required(): ValidationRuleContract {
     name: "required",
     handler: (value: any): boolean => {
       return reallyEmpty(value) === false;
+    },
+  };
+}
+
+export function requiredIf(args: Array<string>): ValidationRuleContract {
+  const argsLen = args.length;
+
+  if (
+    !args
+    || argsLen < 2
+    || argsLen % 2 !== 0
+  ) {
+    throw new Error(`Invalid number of arguments.`);
+  }
+
+  return {
+    name: "requiredIf",
+    handler: (value: any, v: ValidatorContract): boolean => {
+      let required = true;
+      let i = 0;
+
+      for (i; i < argsLen; i += 2) {
+        const attrName = args[i];
+        const requiredAttrVal = args[i + 1];
+        const actualValue = v.attributeValue(attrName);
+
+        if (
+          reallyEmpty(actualValue)
+          || String(requiredAttrVal) !== String(actualValue)) {
+          required = false;
+          break;
+        }
+      }
+
+      // console.log('final con', required, reallyEmpty(value));
+
+      return (required && reallyEmpty(value)) ? false : true;
     },
   };
 }
--- src/rules/required.spec.ts
+++ src/rules/required.spec.ts
@@ -1,4 +1,6 @@
-import { required } from "../rules/required.rule";
+import { ValidatorLite } from '../mock/validator-lite.mock';
+
+import { required, requiredIf } from "../rules/required.rule";
 
 test("rules:required", function (): void {
   const ruleHandler = required().handler;
@@ -11,4 +13,26 @@ test("rules:required", function (): void {
   expect(ruleHandler("")).toBe(false);
   expect(ruleHandler()).toBe(false);
   expect(ruleHandler(null)).toBe(false);
+});
+
+test("rules:requiredIf", function (): void {
+  const inputs = {
+    name: 'Some User',
+    sex: 'male',
+    age: 18,
+  };
+
+  expect(requiredIf(['age', '15']).handler(undefined, new ValidatorLite(inputs))).toBe(true);
+  expect(requiredIf(['age', '15']).handler('', new ValidatorLite(inputs))).toBe(true);
+  expect(requiredIf(['age', '15']).handler(null, new ValidatorLite(inputs))).toBe(true);
+
+  expect(requiredIf(['age', '18']).handler(undefined, new ValidatorLite(inputs))).toBe(false);
+  expect(requiredIf(['age', '18']).handler('', new ValidatorLite(inputs))).toBe(false);
+  expect(requiredIf(['age', '18']).handler(null, new ValidatorLite(inputs))).toBe(false);
+
+  expect(requiredIf(['age', '15', 'sex', 'female']).handler(undefined, new ValidatorLite(inputs))).toBe(true);
+  expect(requiredIf(['age', '18', 'sex', 'female']).handler(undefined, new ValidatorLite(inputs))).toBe(true);
+  expect(requiredIf(['age', '15', 'sex', 'male']).handler(undefined, new ValidatorLite(inputs))).toBe(true);
+
+  expect(requiredIf(['age', '18', 'sex', 'male']).handler(undefined, new ValidatorLite(inputs))).toBe(false);
 });
